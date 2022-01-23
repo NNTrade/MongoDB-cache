08f4fe9c21ab0ed8aa6ea4de175d89784a4fb944
InsonusK <insonus.k@gmail.com>
2022-01-23 16:02:41 +0300

    Change version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ package_arr.append(lib)
 
 setuptools.setup(
     name=lib,
-    version="1.1.0",
+    version="2.0.0",
     author="InsonusK",
     author_email="insonus.k@gmail.com",
     description="Framework for saving cache DataFrame to MongoDB",
